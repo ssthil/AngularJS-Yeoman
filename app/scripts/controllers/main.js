9fa3d09a1d07cd9177302b7dd944769735c16ff6
Senthil Sivaramakrishnan <senthil.sivaramakrishnan@emeriocorp.com>
2014-05-14 15:15:54 +0800

    modified

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,16 +4,21 @@ var app = angular.module('angularjsYoApp', []);
  
  app.controller('MainCtrl', function ($scope) {
  //ng-model directive
-	$scope.name = " Senthil S";
+	$scope.name = 'Senthil S';
 //ng-bind directive
-	$scope.inputValue = "UX Engineer";
+	$scope.inputValue = 'UX Engineer';
 //ng-repeat directive
-    $scope.awesomeThings = [
-      {name:'HTML5 Boilerplate'},
-      {name:'AngularJS'},
-      {name:'Karma'} 
-    ];
-//
-
+	$scope.awesomeThings = [
+	  { name:'HTML5 Boilerplate' },
+	  { name:'AngularJS' },
+	  { name:'Karma' }
+	];
+//ng-repeat directive in table format
+	$scope.persons = [
+	{ name: 'Senthil', email: 'ssthil@gmail.com', phone: '98005761' },
+	{ name: 'Selva', email: 'selva@gmail.com', phone: '96004562' },
+	{ name: 'Raaj', email: 'raaj@gmail.com', phone: '96543201' }
+	];
+	
   });
   
--- test/spec/test.js
+++ test/spec/test.js
@@ -8,6 +8,6 @@
             it('should run here few assertions', function () {
 
             });
-        });
+			});
     });
 })();
